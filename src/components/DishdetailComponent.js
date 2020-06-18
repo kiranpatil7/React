89bd62641d83dd252a83eb398437d9bf6596eb3c
kiran-patil <kpandsatish@gmail.com>
2020-06-18 14:14:33 +0530

    single page application 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,76 +1,89 @@
 import React from 'react';
-import { Card, CardBody, CardTitle, CardText, CardImg } from 'reactstrap';
+import { Card, CardBody, CardTitle, CardText, CardImg, BreadcrumbItem, Breadcrumb } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 function RenderDish({ details }) {
     return (
-        <>
-            <Card>
-                <CardImg src={details.image} alt={details.name} />
-                <CardBody>
-                    <CardTitle>{details.name}</CardTitle>
-                    <CardText>{details.description}</CardText>
-                </CardBody>
-            </Card>
-        </>
+
+        <Card>
+            <CardImg src={details.image} alt={details.name} />
+            <CardBody>
+                <CardTitle>{details.name}</CardTitle>
+                <CardText>{details.description}</CardText>
+            </CardBody>
+        </Card>
+
     );
 }
 function RenderComments({ comments }) {
     //return (<h2>{comments.comment}</h2>);
-
     if (comments != null) {
-        const comment = comments.map((comment) => {
-            return (
-                <div>
-                    <p>{comment.comment}</p>
-                    <p>--{comment.author}, {comment.date}</p>
 
-                </div>
+        const menu = comments.map((com) => {
+            return (
+                <>
+                    <p>{com.comment}</p>
+                    <p>-- {com.author}, {com.date}</p>
+                </>
             );
 
         });
 
         return (
             <div>
-                <h4>Comments</h4>
-
-                {comment}
-
+                {menu}
             </div>
         );
     }
-    else
+    else {
         return (
             <div>Be the first one who writes a comment!</div>
         );
+    }
+
+
+
 }
 
 
 
 
-const DishDetail = (props) => {
-    const detail = props.details;
 
-    if (detail != null) {
+
+const DishDetail = (props) => {
+    if (props.details != null) {
         return (
             <div className="container">
                 <div className="row">
+                    <Breadcrumb>
+
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.details.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.details.name}</h3>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+
                     <div className="col-12 col-md-5 m-1">
-                        <RenderDish details={detail} />
+                        <RenderDish details={props.details} />
                     </div>
                     <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={detail.comments} />
+                        {<RenderComments comments={props.comments} />}
                     </div>
                 </div>
 
-            </div>
+            </div >
 
 
         );
 
     }
     else {
-        return (<div></div>)
+        return (<div><h2>From Menu</h2></div>)
     }
 
 
