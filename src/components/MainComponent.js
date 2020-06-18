337dfd1d95029d56f406ba6d637ca06c53ceb45c
kiran-patil <kpandsatish@gmail.com>
2020-06-18 20:29:27 +0530

    assignment2 and abouts added

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -7,6 +7,7 @@ import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import Header from './HeaderComponent';
 import { LEADERS } from '../shared/leaders';
+import About from './AboutComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
@@ -53,6 +54,7 @@ class Main extends Component {
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
                     <Route path="/menu/:dishId" component={DishWithId} />
+                    <Route path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
                     <Route exact path='/contactus' component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
