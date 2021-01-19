8bf26dd24374781f06b3d990ffa5eb8b4c8af7a5
kiranpatil7 <kpandsatish@gmail.com>
2021-01-19 19:14:22 +0530

    readme changed

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { baseUrl } from '../shared/baseUrl'
-import { Stagger, Fade } from 'react-animation-components'
+import { Fade } from 'react-animation-components'
 
 function RenderLeader({ leaderlist, isLoading, ErrMess }) {
 
